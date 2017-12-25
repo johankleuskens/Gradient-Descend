b55d67bd08957392c901643b8570630a1442ee9f
johankleuskens <johan@melderse.nl>
2017-12-25 15:00:32 +0100

    Added Stochastic Gradient Descent

--- Normal-Equation.py
+++ Normal-Equation.py
@@ -30,22 +30,51 @@ for iteration in range(n_iterations):
 X_new_grad = np.array([[0], [2]])
 X_new_grad_b = np.c_[np.ones((2,1)), X_new_norm]
 y_pred_grad = X_new_grad_b.dot(theta_grad)
-    
 
+# Do a stochastic Gradient Descent
+n_epochs = 50
+t0, t1 = 5, 50
+
+theta_sgd = np.random.randn(2,1)
+
+def learning_schedule(t):
+    return t0/(t + t1)
+    
+for epoch in range(n_epochs):
+    for i in range(m):
+        random_index = np.random.randint(m)
+        xi = X_b[random_index:random_index+1]
+        yi = y[random_index:random_index+1]
+        gradients = 2 * xi.T.dot(xi.dot(theta_sgd) - yi)
+        eta = learning_schedule(i + m* epoch)
+        theta_sgd = theta_sgd - eta * gradients
+
+# Make prediction     
+X_new_sgd = np.array([[0], [2]])
+X_new_sgd_b = np.c_[np.ones((2,1)), X_new_norm]
+y_pred_sgd = X_new_sgd_b.dot(theta_sgd)
+                                 
 # show things
 fig = plt.figure()
 fig.subplots_adjust(hspace=0.5)
 
-ax1 = fig.add_subplot(211)
+ax1 = fig.add_subplot(311)
 ax1.set_title('Normal Equation')
 ax1.plot(X,y,"b.")
 ax1.plot(X_new_norm, y_pred_norm, "r-")
 ax1.axis([0,2,0,15])
 
 
-ax2 = fig.add_subplot(212)
+ax2 = fig.add_subplot(312)
 ax2.set_title('Gradient Descent')
 ax2.plot(X,y,"b.")
 ax2.plot(X_new_grad, y_pred_grad, "y-")
 ax2.axis([0,2,0,15])
+
+ax3 = fig.add_subplot(313)
+ax3.set_title('Stochastic Gradient Descent')
+ax3.plot(X,y,"b.")
+ax3.plot(X_new_grad, y_pred_grad, "p-")
+ax3.axis([0,2,0,15])
+
 plt.show()
